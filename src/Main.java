1f6be22701349ba2b8ad220a1dd55120f3511a68
Simonika <simo.pojar@gmail.com>
2019-05-23 20:50:39 +0300

    test 1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        char myChar = 'a';
+        System.out.println(myChar);
+
+
+
+
+
+
+
+    }
+
+
+}
