1f02ac01387f0029f029691744f7019b706f0770
Simonika <simo.pojar@gmail.com>
2019-05-28 22:01:48 +0300

    Exercises 2-4

---
+++ src/Operations.java
@@ -0,0 +1,22 @@
+public class Operations {
+    public static void main(String args[]){
+        int first = 5;
+        int second = 123;
+        int result = first + second;
+        System.out.println(result);
+
+        double a = 23;
+        double b = 4;
+        double c = a / b;
+        System.out.println(c);
+
+        System.out.println("The result of '-5 + 8 * 6' is "+(-5 + 8 * 6));
+
+        System.out.println("The result of '(55+9) % 9' is "+((55+9) % 9));
+
+        System.out.println("The result of '20 + -3*5 / 8' is "+((20 + -3*5 / 8 )));
+
+        System.out.println("The result of '5 + 15 / 3 * 2 - 8 % 3 ' is "+((5 + 15 / 3 * 2 - 8 % 3)));
+
+    }
+}
