d86c8602e3dd426d32a6f040053c6514c09b56fb
Simonika <simo.pojar@gmail.com>
2019-05-28 20:59:59 +0300

    printing greeting and name

---
+++ src/DemoSimona.java
@@ -0,0 +1,11 @@
+public class DemoSimona {
+
+    public static void main(String a[]){
+        String title = "Hello";
+        System.out.println(title);
+        String name = "Simona";
+        System.out.println(name);
+
+    }
+
+}
