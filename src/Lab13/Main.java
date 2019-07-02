448ffc7e34330ad05387e5cd7b8e91f6878575a2
Simonika <simo.pojar@gmail.com>
2019-07-02 18:49:50 +0300

    Laborator 13 uncomment

--- src/Lab13/Main.java
+++ src/Lab13/Main.java
@@ -9,33 +9,33 @@ public class Main {
 
     public static void main(String[] args) {
         LogicalOp myOp= new LogicalOp();
-//        myOp.afisareArray(myOp.getArrayToHundred());
-//        myOp.afisareArray(myOp.getArrayEvenNumbers());
+        myOp.afisareArray(myOp.getArrayToHundred());
+        myOp.afisareArray(myOp.getArrayEvenNumbers());
 
         //Ex.4
-//        int[] numbers= {1, 7, 9, 67, 9, -65, 77, 123, 2309};
-//        System.out.println(myOp.getAveregeOfArrayNumners(numbers));
+        int[] numbers= {1, 7, 9, 67, 9, -65, 77, 123, 2309};
+        System.out.println(myOp.getAveregeOfArrayNumners(numbers));
 
         //Ex5
-//       String[] animals = {"dog", "cat", "mouse", "horse", "cow", "pig"};
-//        System.out.println(myOp.findStringInArray(animals, "cat"));
+       String[] animals = {"dog", "cat", "mouse", "horse", "cow", "pig"};
+        System.out.println(myOp.findStringInArray(animals, "cat"));
 
         //Ex6
-//        int[] numbers = {6, 9, 15, 42};
-//        System.out.println(  myOp.findNumberInArray(numbers,15));
+        int[] numbers2 = {6, 9, 15, 42};
+        System.out.println(  myOp.findNumberInArray(numbers2,15));
 
         //Ex8
-//        int[] numbers = {34, 21, 9, 4, 10, 3};
-//            System.out.print(Arrays.toString(myOp.removeOneElememFromArray(numbers, 21)));
+        int[] numbers3 = {34, 21, 9, 4, 10, 3};
+            System.out.print(Arrays.toString(myOp.removeOneElememFromArray(numbers3, 21)));
 
         //Ex9
-//        int[] numbers3 = {-9, 34, 21, 9, 4, 10, 3};
-//        System.out.print(myOp.getSecondSmallestFromArray(numbers3));
+        int[] numbers4 = {-9, 34, 21, 9, 4, 10, 3};
+        System.out.print(myOp.getSecondSmallestFromArray(numbers4));
 
         //Ex10
-//        int[] numbers2 = {-9, 34, 21, 1, 9, 4, 10, 3};
-//        int [] newNumbers = new int[numbers2.length];
-//        System.out.print(Arrays.toString(myOp.copyFromArray(numbers2, newNumbers)));
+        int[] numbers5 = {-9, 34, 21, 1, 9, 4, 10, 3};
+        int [] newNumbers = new int[numbers2.length];
+        System.out.print(Arrays.toString(myOp.copyFromArray(numbers5, newNumbers)));
 
 
 
